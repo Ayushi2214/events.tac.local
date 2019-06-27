e4f77d207ffb57c46d7a894dcbc29f2070246aea
Ayushi Bhargava <it.ayushi14@gmail.com>
2019-06-27 11:02:24 +0530

    first commit

---
+++ events.tac.local/Models/OverviewList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace events.tac.local.Models
+{
+    public class OverviewList:List<OverviewItem>
+    {
+        public string ReadMore { get; set; }
+       
+    }
+}
---
+++ events.tac.local/Scripts/commentsForm.js
@@ -0,0 +1,26 @@
+﻿
+
+function createCommentItem(form, path)
+{
+    var service = new Itemservice({ url: 'sitecore/api/ssc/item' });
+    var obj = {
+        ItemName: 'comment -' + form.name.value,
+        TemplateID: '{0849ACE1-3FA5-46C3-8631-59791B539F0C}',
+        Name: form.name.value,
+        Comment: form.comment.value
+
+    };
+    service.create(obj)
+        .path(path)
+        .execute()
+        .then(function (item) {
+            form.name.value = form.comment.value = '';
+            window.alert('thanks.Your message will show on the site shortly');
+        })
+        .fail(function (err) {
+            window.alert(err);
+        });
+    event.preventDefault();
+    return false;
+
+}
